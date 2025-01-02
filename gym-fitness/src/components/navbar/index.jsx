6090fd4fa9089bc276df7be859769096614ce18f
Shaheryar-Ashraf-007 <shaheryarashraf824@gmail.com>
2025-01-03 01:38:25 +0500

    add gym-fitness-folder in javascript

--- gym-fitness/src/components/navbar/index.jsx
+++ gym-fitness/src/components/navbar/index.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react"
-import logo from './../assets/Logo.png'
-import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import logo from './../../assets/Logo.png'
+import { RiMenu3Line } from "react-icons/ri";
+import { motion } from "framer-motion";
 
-type Props = {}
 
-const Navbar = (props: Props) => {
+const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
@@ -18,17 +18,17 @@ const Navbar = (props: Props) => {
           {/* Desktop Menu */}
           <div className="hidden md:block">
             <ul className="flex items-center justify-between p-4 gap-8 cursor-pointer">
-              <li className="hover:text-yellow-400">Home</li>
-              <li className="hover:text-yellow-400">Benefits</li>
-              <li className="hover:text-yellow-400">Our Classes</li>
-              <li className="hover:text-yellow-400">Contact Us</li>
+              <li className="hover:text-primary-500">Home</li>
+              <li className="hover:text-primary-500">Benefits</li>
+              <li className="hover:text-primary-500">Our Classes</li>
+              <li className="hover:text-primary-500">Contact Us</li>
             </ul>
           </div>
 
           {/* Desktop Buttons */}
           <div className="hidden md:block">
             <ul className="flex items-center justify-between gap-6 h-16 cursor-pointer">
-              <li className="hover:text-yellow-400">Sign In</li>
+              <li className="hover:text-primary-500">Sign In</li>
               <button className="bg-yellow-400 h-12 px-4 w-72 rounded-md hover:bg-yellow-500 transition duration-300">
                 Become a Member
               </button>
@@ -46,18 +46,23 @@ const Navbar = (props: Props) => {
 
         {/* Mobile Menu */}
         {isMenuOpen && (
-          <div className="md:hidden absolute top-16 left-0 right-0 bg-gray-20 z-50">
+          <motion.div
+          initial={{ x: 200, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          exit={{ x: 200, opacity: 0 }}
+          transition={{ duration: 0.5 }}
+           className=" h-full md:hidden absolute top-16 left-0 right-0 bg-gray-20 z-50 ">
             <ul className="flex flex-col items-center gap-4 py-4">
-              <li className="hover:text-yellow-400">Home</li>
-              <li className="hover:text-yellow-400">Benefits</li>
-              <li className="hover:text-yellow-400">Our Classes</li>
-              <li className="hover:text-yellow-400">Contact Us</li>
-              <li className="hover:text-yellow-400">Sign In</li>
+              <li className="hover:text-primary-500">Home</li>
+              <li className="hover:text-primary-500">Benefits</li>
+              <li className="hover:text-primary-500">Our Classes</li>
+              <li className="hover:text-primary-500">Contact Us</li>
+              <li className="hover:text-primary-500">Sign In</li>
               <button className="bg-yellow-400 h-12 px-4 w-72 rounded-md hover:bg-yellow-500 transition duration-300">
                 Become a Member
               </button>
             </ul>
-          </div>
+          </motion.div>
         )}
       </div>
     </nav>
